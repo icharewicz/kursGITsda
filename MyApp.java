1026d6349edf9833457e53461c81388c6eea22f3
i.charewicz <ikharewicz@gmail.com>
2022-01-18 22:16:54 +0100

    2022-01-18

---
+++ MyApp.java
@@ -0,0 +1,11 @@
+public class MyApp {
+	
+	public static void main (String[] args) {
+		System.out.println("Hello, World!");
+		String[] words = {"Ala", " ma", " Kota"};
+		//System.out.println(words[3]);
+		final String a = "aa";
+		a = "b";
+		a.toUpperCase();
+	}
+}
